142aa79cdd0bb4aa86870db3afdaca5b740fbb23
Safari <safarihotelmanager@gmail.com>
2026-02-05 21:50:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9100871840-38c65",
+  "appId": "1:1019582287277:web:9f43a5166d5a0a5be9a524",
+  "apiKey": "AIzaSyA7Jq8QOPPbuGN9jKekUAYyoiepFMVYl4c",
+  "authDomain": "studio-9100871840-38c65.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1019582287277"
+};
